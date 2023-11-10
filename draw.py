b8c75c8d95124720004287d2b4b2d4b84e3c9f58
yzdnaufan <ahyaznau@gmail.com>
2023-11-10 20:40:34 +0700

    feat : near complete

---
+++ draw.py
@@ -0,0 +1,45 @@
+
+import cv2
+import numpy as np
+import base64
+
+
+def draw_boxes(bytes_image : bytes, result):
+
+    nparr = np.frombuffer(bytes_image, np.uint8)
+
+    img_cv = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    height, width, _ = img_cv.shape
+
+    # Iterate over the detected objects
+    for obj in result['data'][0:2]:
+        # Get the bounding box coordinates
+
+
+        x, y, w, h = obj['box'].values()
+
+        x1 = int(x) - int(w/2) if int(x) - int(w/2) > 0 else 0
+        y1 = int(y) - int(h/2) if int(y) - int(h/2) > 0 else 0
+
+        x2 = int(x) + int(w/2) if int(x) + int(w/2) < width else width
+        y2 = int(y) + int(h/2) if int(y) + int(h/2) < height else height
+
+        # Draw the bounding box
+        # cv2.rectangle(img_cv,(int(x),int(y)),(int(w), int(h)),  lineType=cv2.LINE_AA, color=(0, 255, 0), thickness=2)
+        cv2.rectangle(img_cv,(x1,y1),(x1 + x2, y2),  lineType=cv2.LINE_AA, color=(0, 255, 0), thickness=2)
+
+        # Optionally, add a label
+        # label = obj['label']
+        # cv2.putText(img_cv, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+    # Assuming 'image' is your OpenCV image
+    retval, buffer = cv2.imencode('.jpg', img_cv)
+
+    # Convert to base64
+    img_base64 = base64.b64encode(buffer).decode()
+
+    # If you want to get a data URL
+    img_data_url = 'data:image/jpeg;base64,' + img_base64
+    
+    return img_data_url, width, height
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from google.cloud import secretmanager
+
+import json
+import base64
+import requests
+import PIL.Image as Image
+import io
+
+from img import img_64
+from draw import draw_boxes
 
 app = FastAPI()
 
@@ -13,6 +25,73 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class Data(BaseModel):
+    image: str
+
+# GET request
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# POST request
+
+@app.post("/demo")
+# def receive_data( data: Data):
+async def receive_data():
+
+    # 1. Receive data from the client
+    # image64 = data.image
+    image64 = img_64.split(",")[1]
+
+    # 2. Process the data
+    count = 0
+    result_b64 = "base64 image"
+
+    # Create the Secret Manager client.
+    # client = secretmanager.SecretManagerServiceClient()
+
+    # Run inference on an image
+    # api_key = client.access_secret_version(request={"name": "ULTRALYTICS_SECRET"})
+
+    url = "https://api.ultralytics.com/v1/predict/ipyo4cywDcA7LgB4Zy1n"
+    headers = {"x-api-key": "b72997cc481dfa7aa0c6c856c8532ecb301ef68f55"}
+
+    data = {"size": 640, "confidence": 0.25, "iou": 0.45}
+
+    # with open("path/to/image.jpg", "rb") as f:
+    #     re = 1       
+    
+    img_decoded = io.BytesIO(base64.b64decode(image64))
+
+    response = requests.post(url, headers=headers, data=data, files={"image": img_decoded})
+
+    # Check for successful response
+    response.raise_for_status()
+
+    # Print inference results
+    # print(json.dumps(response.json(), indent=2))
+
+    # Draw bounding boxes and labels on image
+    # result = response.json()
+    img_decoded.seek(0)
+    im = img_decoded.read()
+
+    result = response.json()
+
+    b64_result, w, h = draw_boxes(im, result)
+
+    x, y, x2, y2 = response.json()["data"][0]['box'].values()
+
+    # 3. Send some data back to the client
+    return {
+         "x": x,
+        "y": y,
+        "x2": x2,
+        "y2": y2,
+        "witdh" : w,
+        "height" : h,
+        "count": len(response.json()["data"]),
+        "image": b64_result
+    }
